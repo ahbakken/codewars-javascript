0e4542b6e6636b67650bd64210559aa054645ddc
Astrid Bakken <astridbakken95@gmail.com>
2023-03-05 18:06:46 +0100

    feat: question 1

--- index.js
+++ index.js
@@ -3,17 +3,16 @@
 // (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. 
 // Spaces will be included only when more than one word is present.
 function spinWords(string){
-    const words = string.split(" ");
-    
-    const reversedWords = words.map(word => {
-      if (word.length >= 5) {
-        return word.split("").reverse().join("");
-      } else {
-        return word;
-      }
-    });
-    
-    return reversedWords.join(" ");
-  }
+  const myArray = string.split(' ');
+  let revArray = []
+  myArray.map(word => {
+    if (word.length >= 5) {
+      revArray.push(word.split('').reverse().join(''))
+    }
+    else { revArray.push(word) }
+  })
+  return revArray.join(' ')
+}
 
 spinWords("Hey fellow warriors") // "Hey wollef sroirraw"
+console.log(spinWords("Hey fellow warriors"))
